b373e5a91646eb834c1b0644177b113cfef1d376
Paul Thomas <paulthomas2107@gmail.com>
2022-09-12 16:39:37 +0100

    More

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,7 +1,7 @@
 import express from 'express';
 
 import MessageResponse from '../interfaces/MessageResponse';
-import emojis from './emojis';
+
 import todos from './todos/todos.routes';
 
 const router = express.Router();
@@ -12,7 +12,6 @@ router.get<{}, MessageResponse>('/', (req, res) => {
   });
 });
 
-router.use('/emojis', emojis);
 router.use('/todos', todos);
 
 export default router;
